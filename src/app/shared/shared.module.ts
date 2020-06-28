370999989f122bc41aa84713f23f1d1bbdc7c5dd
WRayz <ruizhouwu@gmail.com>
2020-06-28 23:07:53 +0800

    Header調整，出現漢堡

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent, FooterComponent],
-  imports: [CommonModule, MatDividerModule],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+  ],
   exports: [HeaderComponent, SidebarComponent, FooterComponent],
 })
 export class SharedModule {}
